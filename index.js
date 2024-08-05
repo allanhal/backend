23185a9a2b8fd12e2a3b1f5e2242181ce8036390
Visitante Digital <visitante.digital@DIGITALCOLLEGE.LOCAL>
2024-08-05 11:57:48 -0300

    chuchu

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(cors())
+
+app.get('/v1/user/:id', (request, res) => {
+    console.log('request.url', request.url) // debug
+    console.log('request.params.id', request.params.id)
+
+    // COMECO DO CODIGO BRABO
+    const mysql = require('mysql2');
+
+    // Crie uma conexão com o banco de dados
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'dc'
+    });
+
+    // Conecte-se ao banco de dados
+    connection.connect((err) => {
+        if (err) {
+            return console.error('Erro ao conectar: ' + err.stack);
+        }
+        console.log('Conectado como id ' + connection.threadId);
+
+
+        // Execute uma consulta SQL
+        connection.query('select * from dc.usuarios where id='+request.params.id+';', (err, results, fields) => {
+            if (err) {
+                console.error('Erro ao executar consulta: ' + err.stack);
+                return;
+            }
+            console.log('Resultados da consulta:', results);
+            res.send(results)
+        });
+
+    });
+
+
+    // FIM DO CODIGO BRABO
+    // 'select * from dc.usuarios where id=1;'
+    // res.send({
+    //     "id": 1,
+    //     firstname: "primeiro nome",
+    //     surname: 'segundo nome',
+    //     email: 'email@email.com',
+    //     password: 1234
+    // })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
---
+++ index2.js
@@ -0,0 +1,29 @@
+const mysql = require('mysql2');
+
+// Crie uma conexão com o banco de dados
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'dc'
+});
+
+// Conecte-se ao banco de dados
+connection.connect((err) => {
+  if (err) {
+    return console.error('Erro ao conectar: ' + err.stack);
+  }
+  console.log('Conectado como id ' + connection.threadId);
+
+
+  // Execute uma consulta SQL
+  connection.query('select * from dc.usuarios where id=1;', (err, results, fields) => {
+    if (err) {
+      console.error('Erro ao executar consulta: ' + err.stack);
+      return;
+    }
+    console.log('Resultados da consulta:', results);
+  });
+
+});
+
