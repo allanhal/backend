7519c9887868f6f5bcef34d2aae826bb7252507c
unknown <ADM@DESKTOP-MRU2OC1.DIGITALCOLLEGE.LOCAL>
2024-08-07 11:58:13 -0300

    chuchu

--- index.js
+++ index.js
@@ -1,62 +1,38 @@
 const express = require('express')
 var cors = require('cors')
+// DESESTRUTURACAO
+const { Sequelize, DataTypes, QueryTypes } = require('sequelize');
 const app = express()
-const port = 10000
+const port = 9999
 
+const sequelize = new Sequelize('postgres', 'postgres.yhogvuqcegpuxhumgkyn', '', {
+    host: "aws-0-us-west-1.pooler.supabase.com",
+    dialect: 'postgres',
+    port: 6543,
+    logging: true, // Desative o log de SQL se não precisar
+});
 app.use(cors())
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Olá, mundo')
 })
-
-
-// https://pagamento.inter.com/pagamento?tipo=boleto&valor=100&pedido=100&desconto
+// /v1/user/chuchu METODOS HTTP
 
 app.get('/v1/user/:id', (request, res) => {
     console.log('request.url', request.url) // debug
     console.log('request.params.id', request.params.id)
 
-    // COMECO DO CODIGO BRABO
-    const mysql = require('mysql2');
+    sequelize.query('SELECT * FROM users where id="' + request.params.id + '"', {
+        type: QueryTypes.SELECT,
+    }).then((result) => res.send(result));
+})
+app.post('/v1/user/:name', (request, res) => {
+    console.log('request.url', request.url) // debug
+    console.log('request.params.name', request.params.name)
 
-    // Crie uma conexão com o banco de dados
-    const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'dc'
-    });
-
-    // Conecte-se ao banco de dados
-    connection.connect((err) => {
-        if (err) {
-            return console.error('Erro ao conectar: ' + err.stack);
-        }
-        console.log('Conectado como id ' + connection.threadId);
-
-
-        // Execute uma consulta SQL
-        connection.query('select * from dc.usuarios where id='+request.params.id+';', (err, results, fields) => {
-            if (err) {
-                console.error('Erro ao executar consulta: ' + err.stack);
-                return;
-            }
-            console.log('Resultados da consulta:', results);
-            res.send(results)
-        });
-
-    });
-
-
-    // FIM DO CODIGO BRABO
-    // 'select * from dc.usuarios where id=1;'
-    // res.send({
-    //     "id": 1,
-    //     firstname: "primeiro nome",
-    //     surname: 'segundo nome',
-    //     email: 'email@email.com',
-    //     password: 1234
-    // })
+    sequelize.query("INSERT INTO users (name, email) values ('"+request.params.name+"', '"+request.params.name+"')", {
+        type: QueryTypes.INSERT,
+    }).then((result) => res.status(201).send(result));
 })
 
 app.listen(port, () => {
