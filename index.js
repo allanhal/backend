65cb6f9f06659ead1809aaa548f07e78db5212be
techcomkathia <techcomk@gmail.com>
2024-08-08 08:36:49 -0300

    tudo rodando liso

--- index.js
+++ index.js
@@ -3,14 +3,9 @@ var cors = require('cors')
 // DESESTRUTURACAO
 const { Sequelize, DataTypes, QueryTypes } = require('sequelize');
 const app = express()
-const port = 9999
+const port = 10000
 
-const sequelize = new Sequelize('postgres', 'postgres.yhogvuqcegpuxhumgkyn', '', {
-    host: "aws-0-us-west-1.pooler.supabase.com",
-    dialect: 'postgres',
-    port: 6543,
-    logging: true, // Desative o log de SQL se não precisar
-});
+const sequelize = new Sequelize('postgresql://postgres.yhogvuqcegpuxhumgkyn:'+process.env.DATABASE_PASSWORD+'@aws-0-us-west-1.pooler.supabase.com:6543/postgres');
 app.use(cors())
 
 app.get('/', (req, res) => {
@@ -22,7 +17,7 @@ app.get('/v1/user/:id', (request, res) => {
     console.log('request.url', request.url) // debug
     console.log('request.params.id', request.params.id)
 
-    sequelize.query('SELECT * FROM users where id="' + request.params.id + '"', {
+    sequelize.query("SELECT * FROM users where id='" + request.params.id + "'", {
         type: QueryTypes.SELECT,
     }).then((result) => res.send(result));
 })
