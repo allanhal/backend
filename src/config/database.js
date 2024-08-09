a85fba62cd82ca7b49482a1c1def32cd7f617b54
techcomkathia <techcomk@gmail.com>
2024-08-09 10:59:42 -0300

    errei, fui neymar

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,3 @@
 module.exports = {
   uri: 'postgresql://'+process.env.USUARIO+':'+process.env.SENHA+'@'+process.env.HOST+':6543/postgres'
 }
-
-
-// USUARIO=postgres.yhogvuqcegpuxhumgkyn
-// SENHA=chuchu-banco-vai-dar-bom
-// HOST=aws-0-us-west-1.pooler.supabase.com
